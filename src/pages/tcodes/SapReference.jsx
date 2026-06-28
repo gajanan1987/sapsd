ae33759c2758381566ac7cb7b39ed4572d3af613
gajanan1987 <nehete.gaju@gmail.com>
2026-06-28 19:29:48 +0530

    updates

--- src/pages/tcodes/SapReference.jsx
+++ src/pages/tcodes/SapReference.jsx
@@ -2,9 +2,15 @@ import SapReferenceTable from "./components/SapReferenceTable";
 import { sapSdReferenceData } from "../../data/sapSdReferenceData";
 
 const SapReference = () => {
+  const formatTitle = (str) =>
+    str
+      .replace(/([A-Z])/g, " $1") // Add space before capital letters
+      .replace(/^./, (c) => c.toUpperCase()) // Capitalize first letter
+      .trim();
+
   const referenceData = Object.entries(sapSdReferenceData).map(
     ([key, value]) => ({
-      title: key,
+      title: formatTitle(key),
       classes: key,
       columns: value[0] ? Object.keys(value[0]) : [],
       rows: value.map((item) => Object.values(item)),
