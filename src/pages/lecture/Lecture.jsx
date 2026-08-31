79a70c4c0083119fac403fab4e3b0bdae3d98822
gajanan1987 <nehete.gaju@gmail.com>
2026-08-31 17:23:00 +0530

    bug fixes

--- src/pages/lecture/Lecture.jsx
+++ src/pages/lecture/Lecture.jsx
@@ -1,21 +1,11 @@
-// import { useCallback, useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { useAuth } from "../../hooks/useAuth";
-// import {
-//   fetchDefinition,
-//   deleteComp,
-//   compDetails,
-//   clearCompDetails,
-// } from "../../redux/definationSlice";
-// import custMessage from "../../utils/toast";
-// import CompanyCard from "./component/CompanyCard";
-// import Banner from "./component/Banner";
 import Pricing1 from "./pricing/Pricing1";
 
 const Lecture = () => {
   return (
     <>
-      <Pricing1 />
+      <div>
+        <Pricing1 />
+      </div>
     </>
   );
 };
